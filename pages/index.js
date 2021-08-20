6c9b4b166dff7bb43f3ec711dd7cef0dc35e13a1
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2021-08-20 18:15:56 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Index() {
+  return (
+    <div>
+      <Head>
+        <title>:: RRS ::.</title>
+        <meta name="description" content="rodneyrinaldi services" />
+      </Head>
+      <p>RRS ...</p>
+    </div>
+  )
+}
